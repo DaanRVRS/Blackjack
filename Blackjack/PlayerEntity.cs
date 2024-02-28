d7cfb6179b17002ddc39b85c41d75d0bf094fc53
Daan Reuvers <77575704+DaanRVRS@users.noreply.github.com>
2024-02-28 14:32:37 +0100

    added player entity

---
+++ Blackjack/PlayerEntity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackjack
+{
+    internal class PlayerEntity
+    {
+
+        public PlayerEntity(int id, int bet, int cardsPulled)
+        {
+            playerId = id;
+            playerBet = bet;
+            playerCardsPulled = cardsPulled;
+        }
+
+        public int playerId { get; }
+        public int playerBet { get; }
+        public int playerCardsPulled { get; }
+    }
+}
